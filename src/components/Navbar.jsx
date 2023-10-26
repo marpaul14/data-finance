4b9aed7e13f5b2ce13dd6e96922b6560c8c2608f
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-10-26 22:27:53 +0800

    added hero component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,40 @@
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
 
 function Navbar() {
+  const [nav, setNav] = useState(false);
+
+  function handleNav() {
+    setNav(!nav);
+  }
+
   return (
     <div className="text-white flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4">
       <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
-      <ul className="flex hidden">
+      <ul className="hidden md:flex">
         <li className="p-4">Home</li>
         <li className="p-4">Company</li>
         <li className="p-4">Resources</li>
         <li className="p-4">About</li>
         <li className="p-4">Contact</li>
       </ul>
-      <div>
-        <AiOutlineMenu size={20} />
+      <div onClick={handleNav} className="block md:hidden">
+        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-      <div className="fixed left-0 top-0 w-[60%] border-r border-r-gray-900">
-        <ul>
-          <li className="p-4">Home</li>
-          <li className="p-4">Company</li>
-          <li className="p-4">Resources</li>
-          <li className="p-4">About</li>
-          <li className="p-4">Contact</li>
+      <div
+        className={
+          !nav
+            ? "fixed left-0 top-0 w-[60%] ease-in-out duration-500 bg-[#000300] border-r border-r-gray-900 h-full"
+            : "fixed left-[-100%]"
+        }
+      >
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">REACT.</h1>
+        <ul className="p-4 uppercase">
+          <li className="p-4 border-b border-gray-600">Home</li>
+          <li className="p-4 border-b border-gray-600">Company</li>
+          <li className="p-4 border-b border-gray-600">Resources</li>
+          <li className="p-4 border-b border-gray-600">About</li>
+          <li className="p-4 ">Contact</li>
         </ul>
       </div>
     </div>
