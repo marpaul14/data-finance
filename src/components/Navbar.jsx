d1ae640cdd88041b16bf912af875ccdb92162363
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-10-26 21:15:42 +0800

    Second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,28 @@
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
 function Navbar() {
   return (
-    <div className="text-white">
-      <h1 className="">REACT.</h1>
+    <div className="text-white flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4">
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+      <ul className="flex hidden">
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Resources</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
+      <div>
+        <AiOutlineMenu size={20} />
+      </div>
+      <div className="fixed left-0 top-0 w-[60%] border-r border-r-gray-900">
+        <ul>
+          <li className="p-4">Home</li>
+          <li className="p-4">Company</li>
+          <li className="p-4">Resources</li>
+          <li className="p-4">About</li>
+          <li className="p-4">Contact</li>
+        </ul>
+      </div>
     </div>
   );
 }
