febe4770a15b4b8b060b24e0de53c5893df58e44
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-10-27 19:38:29 +0800

    cards component added

---
+++ src/components/Cards.jsx
@@ -0,0 +1,9 @@
+function Cards() {
+  return (
+    <div className="w-full py-[10rem] px-4 bg-white">
+      <div className="max-w-[1240px] mx-auto grid grid-cols-3 gap-8"></div>
+    </div>
+  );
+}
+
+export default Cards;
