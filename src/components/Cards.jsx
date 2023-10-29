98c93309e54725fcb128b9b7f7fdfa4a95810613
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-10-29 17:32:35 +0800

    cards component done

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,7 +6,7 @@ function Cards() {
   return (
     <div className="w-full py-[10rem] px-4 bg-white">
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
-        <div className="w-full shadow-xl flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
           <img
             className="w-20 mx-auto mt-[-3rem] bg-white"
             src={Single}
@@ -14,35 +14,33 @@ function Cards() {
           />
           <h2 className="text-2xl font-bold text-center py-8">Single User</h2>
           <p className="text-center text-4xl font-bold">$149</p>
-          <div className="text-center front-medium">
+          <div className="text-center font-medium">
             <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
-            <p className="py-2 border-b mx-8 ">1 Granted User</p>
-            <p className="py-2 border-b mx-8 ">Send up to 2 GB</p>
+            <p className="py-2 border-b mx-8">1 Granted User</p>
+            <p className="py-2 border-b mx-8">Send up to 2 GB</p>
           </div>
           <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">
             Start Trial
           </button>
         </div>
-
-        <div className="w-full shadow-xl flex-col p-4 my-8 md:my-0 rounded-lg hover:scale-105 duration-300">
+        <div className="w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300">
           <img
-            className="w-20 mx-auto mt-[-3rem] bg-white"
+            className="w-20 mx-auto mt-[-3rem] bg-transparent"
             src={Double}
             alt="/"
           />
           <h2 className="text-2xl font-bold text-center py-8">Single User</h2>
           <p className="text-center text-4xl font-bold">$149</p>
-          <div className="text-center front-medium">
+          <div className="text-center font-medium">
             <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
-            <p className="py-2 border-b mx-8 ">1 Granted User</p>
-            <p className="py-2 border-b mx-8 ">Send up to 2 GB</p>
+            <p className="py-2 border-b mx-8">1 Granted User</p>
+            <p className="py-2 border-b mx-8">Send up to 2 GB</p>
           </div>
           <button className="bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">
             Start Trial
           </button>
         </div>
-
-        <div className="w-full shadow-xl flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
           <img
             className="w-20 mx-auto mt-[-3rem] bg-white"
             src={Triple}
@@ -50,10 +48,10 @@ function Cards() {
           />
           <h2 className="text-2xl font-bold text-center py-8">Single User</h2>
           <p className="text-center text-4xl font-bold">$149</p>
-          <div className="text-center front-medium">
+          <div className="text-center font-medium">
             <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
-            <p className="py-2 border-b mx-8 ">1 Granted User</p>
-            <p className="py-2 border-b mx-8 ">Send up to 2 GB</p>
+            <p className="py-2 border-b mx-8">1 Granted User</p>
+            <p className="py-2 border-b mx-8">Send up to 2 GB</p>
           </div>
           <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">
             Start Trial
